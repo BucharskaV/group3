4eef3490019572a8ba43176ec06558873085081c
Mari Kibets <mariakibec7@Gmail.com>
2025-11-17 21:40:22 +0100

    Combine of ExtentContainer.cs and ExtendPersistence.cs

--- HotelBounty/Persistence/ExtentContainer.cs
+++ HotelBounty/Persistence/ExtentContainer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Xml.Serialization;
 using HotelBounty.Employees;
+using HotelBounty.Rooms;
 
 namespace HotelBounty.Persistence;
 
@@ -10,4 +11,8 @@ public class ExtentContainer
     [XmlArray("Employees")]
     [XmlArrayItem("Employee")]
     public List<Employee> Employees { get; set; } = new List<Employee>();
+    
+    [XmlArray("Rooms")]
+    [XmlArrayItem("Room")]
+    public List<Room> Rooms { get; set; } = new List<Room>();
 }
